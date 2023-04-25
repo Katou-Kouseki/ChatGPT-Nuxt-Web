d920b32ba91e966d9a566b281659bf7e67e5fea1
Kouseki <30703483+Katou-Kouseki@users.noreply.github.com>
2023-04-25 11:34:03 +0800

    Update index.vue

--- src/views/chat/index.vue
+++ src/views/chat/index.vue
@@ -1,6 +1,4 @@
 <script setup lang='ts'>
-import { useMeta } from 'nuxt3'
-useMeta ( { title: 'ChatGPT - 首页' })
 import type { Ref } from 'vue'
 import { computed, onMounted, onUnmounted, ref } from 'vue'
 import { useRoute } from 'vue-router'
