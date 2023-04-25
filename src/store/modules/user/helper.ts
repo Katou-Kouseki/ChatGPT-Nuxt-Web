85f138ce8b07661023cf0e44433104657002dddd
Kouseki <30703483+Katou-Kouseki@users.noreply.github.com>
2023-04-25 10:13:49 +0800

    Update helper.ts

--- src/store/modules/user/helper.ts
+++ src/store/modules/user/helper.ts
@@ -16,8 +16,8 @@ export function defaultSetting(): UserState {
   return {
     userInfo: {
       avatar: 'https://raw.githubusercontent.com/Chanzhaoyu/chatgpt-web/main/src/assets/avatar.jpg',
-      name: 'ChenZhaoYu',
-      description: 'Star on <a href="https://github.com/Chanzhaoyu/chatgpt-bot" class="text-blue-500" target="_blank" >Github</a>',
+      name: 'You',
+      description: '我的 <a href="https://kouseki.cn/" class="text-blue-500" target="_blank" >主页</a>',
     },
   }
 }
